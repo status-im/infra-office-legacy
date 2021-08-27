1d919b2fa39fbf00a9faf9e4863a273719cd204d
Jakub Sokołowski <jakub@status.im>
2021-08-27 13:47:34 +0200

    bootstrap terraform
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ variables.tf
@@ -0,0 +1,11 @@
+/* GENERAL --------------------------------------*/
+
+variable "public_domain" {
+  description = "Domain under which the public sites go."
+  default     = "status.im"
+}
+
+variable "domain" {
+  description = "DNS Domain to update"
+  default     = "statusim.net"
+}
---
+++ versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.0.0"
+  required_providers {
+    pass = {
+      source  = "camptocamp/pass"
+      version = " = 2.0.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = " = 2.21.0"
+    }
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+    }
+  }
+}
