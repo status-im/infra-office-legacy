93f35a576ae35642d036a9f74b9ec8979837fd58
Jakub Sokołowski <jakub@status.im>
2022-01-18 19:25:49 +0100

    add node-01.do-ams3.crm.office host
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ crm.tf
@@ -0,0 +1,20 @@
+/* Ambassador Program CRM */
+
+module "crm" {
+  source = "github.com/status-im/infra-tf-digital-ocean"
+
+  env    = "crm"
+  group  = "crm"
+  type   = "s-1vcpu-2gb"
+  domain = var.domain
+
+  open_tcp_ports = ["80", "443"]
+}
+
+resource "cloudflare_record" "crm" {
+  zone_id = local.zones["status.im"]
+  name    = "admin-ambassador"
+  type    = "A"
+  proxied = true
+  value   = module.crm.public_ips[0]
+}
